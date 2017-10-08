9aeb0574e7ede4319b4bf674174ad96696e10814
Diba Mirza <dimirza@cs.ucsb.edu>
2017-10-08 11:55:41 -0700

    lec 1

---
+++ lec01/helloCS8.py
@@ -0,0 +1,8 @@
+# The pound symbol precedes a 'comment' in a program
+# Comments provide useful information about the program
+# Comments are not instructions and are simply ignored by the python shell
+
+print('Hello CS 8 Students!')
+print('Aren\'t you having a wonderful time?')
+print('The sum of two and fifty five is', 2+55)
+print('Did you know that the answer to the universe, life and everything is', 42)
