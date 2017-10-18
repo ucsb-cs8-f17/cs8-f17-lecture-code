599265c5ef241cb5866d6137b9027083b6333735
Phill Conrad <pconrad@cs.ucsb.edu>
2017-10-18 12:47:56 -0700

    lecture wip

---
+++ lec06/enterTwoNumbers.py
@@ -0,0 +1,14 @@
+# Entering two numbers
+# Kind of boring, but a good starting point
+
+if __name__=="__main__":
+    print("Please enter a number")
+    num1AsString = input("First number: ")
+
+    print("Please enter another number")
+    num2AsString = input("Second number: ")
+
+    print("You entered:")
+    print("first number=",num1AsString)
+    print("second number=",num2AsString)
+    
---
+++ lec06/tryToAddTwoNumbers.py
@@ -0,0 +1,16 @@
+# Try to add two numbers
+# But this might not do what we want..
+
+if __name__=="__main__":
+    print("Please enter a number")
+    num1 = input("First number: ")
+
+    print("Please enter another number")
+    num2 = input("Second number: ")
+
+    print("You entered:")
+    print("first number=",num1)
+    print("second number=",num2)
+    
+    print("the sum is: ")
+    print(num1 + num2)
