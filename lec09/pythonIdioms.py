89df348d577a63d93077f7d584086bd958f7a89a
Diba Mirza <dimirza@cs.ucsb.edu>
2017-11-01 10:12:59 -0700

    lec9

---
+++ lec09/pythonIdioms.py
@@ -0,0 +1,63 @@
+# English to Python
+
+##############################################
+# Basic loop patterns
+
+# 1. Print the powers of two of all elements of a list (Iteration loop pattern)
+# Iterate through the list 
+#    print each element
+
+# 2. Print every other elememt of a list  (Counter loop pattern)
+# Iterate through the list
+#   print every other element
+
+# 3. Print all elements of a list- two at a time
+# Which loop pattern should we use?
+# A. Iteration loop pattern
+# B. Counter loop pattern
+###################################################################
+# Searching a list
+#
+# 4a. Check if a keyword exists in the list
+#
+# Inputs: (1) list (ll) containing elements of any type
+#         (2) keyword (key) 
+# Output: Return Boolean result True if keyword exists, False otherwise
+# Algorithm: 
+# For each element e in ll
+#    if e is equal to key, return True
+#    else return False
+# Is there a  bug in the above algorithm?
+
+# 4b. Print a message m, if a keyword (key) exists in the list
+#
+# Inputs: (1) list (ll) containing elements of any type
+#         (2) keyword (key) (a value that may or may not exist in the list)
+#         (3) message (m)
+# Output: Print m if at leats one occurence of keyword exists
+
+# How would you modify the above code?
+
+# 5. Print all the elements of a list with value less than (val)
+
+################################################################
+# Accumulator Pattern : Very important!!! 
+# Accumulate (add to, pile on , build on) "something" in every iteration of a loop
+
+# 6. Find the sum of all the elements
+# Input: list (ll) containing elements of numeric type
+# Output: Sum of all the elements
+#
+# 
+#
+
+
+
+
+
+
+
+
+
+
+
