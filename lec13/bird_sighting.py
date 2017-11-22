ef895245ef3430b988632e14bfd10483c11bbf3b
Diba Mirza <dimirza@cs.ucsb.edu>
2017-11-21 19:26:23 -0800

    lec 13

--- lec13/bird_sighting.py
+++ lec13/bird_sighting.py
@@ -8,6 +8,15 @@ def new_sighting_0(kinds, counts, sighting):
   ind = kinds.index(sighting)  
   counts[ind] = counts[ind] + 1
 
+def new_sighting_1(bird_dict, sighting):
+  if sighting not in bird_dict:
+    bird_dict[sighting] = 1
+  else:
+    bird_dict[sighting] = bird_dict[sighting] +1
+
+  
+
+
 def test_new_sighting_0():
   kinds = ['falcon','hawk','sparrow','woodpecker']
   counts = [5, 8, 2, 1]
@@ -15,8 +24,4 @@ def test_new_sighting_0():
   if counts[2]!=3:
     assert(False)
     
-def new_sighting_1(bird_dict, sighting):
-  '''(dict, str) -> NoneType
-  Add new sighting to a dictionary of birds and their counts
-  '''
-  return
+
